a8333cf7ad8b449d5aaaa98e6913a019d678f710
刘攀 <liupan@qmai.cn>
2024-01-05 23:55:39 +0800

    init: bug repro

--- config.mjs
+++ config.mjs
@@ -1,7 +1,7 @@
 import path from "path";
 import { fileURLToPath } from "url";
 import HtmlWebpackPlugin from "html-webpack-plugin";
-
+import { VueLoaderPlugin } from "vue-loader";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const isRunningWebpack = !!process.env.WEBPACK;
 const isRunningRspack = !!process.env.RSPACK;
@@ -16,15 +16,28 @@ const config = {
   mode: "development",
   devtool: false,
   entry: {
-    main: "./src/index",
+    main: "./src/main.js",
   },
-  plugins: [new HtmlWebpackPlugin()],
-  output: {
-    clean: true,
-    path: isRunningWebpack
-      ? path.resolve(__dirname, "webpack-dist")
-      : path.resolve(__dirname, "rspack-dist"),
-    filename: "[name].js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+    new VueLoaderPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: [
+          {
+            loader: "vue-loader",
+            options: {
+              experimentalInlineMatchResource: true,
+            },
+          },
+        ],
+      },
+    ],
   },
   experiments: {
     css: true,
---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      aaa: [
+        {
+          bbb: {
+          },
+        },
+      ],
+    };
+  },
+  mounted() {
+    const index = 0
+    this.aaa[index].bbb.ccc = 1;
+    console.log(this.aaa[index].bbb.ccc);
+  },
+};
+</script>
